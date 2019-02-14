559bf5a7763c97369835acdf873d51120a43ba82
Joshua Larsen <larsen.bass@gmail.com>
2019-02-14 16:47:38 -0700

    beta 1.2 8_ball added randomized colors

--- 8_ball.rb
+++ 8_ball.rb
@@ -1,12 +1,17 @@
-requires "colorize"
+require "colorize"
 
 @answer_key = ["Yes", "No", "Keep Dreaming Cueball", "You Better Put Your Lucky Socks On", "HAHA! NOOB! Definitely Not", "Yeah, I could see that", "Maybe in another Dimension", "You Win, Everytime, With No Exeption"]
-
+@random_colors = [:red, :green, :yellow, :cyan, :white, :blue,]
 def launch
-    puts "Welcome to the Scientifically Accurate 8 Ball. Absolutely No Magic Here!"
-    puts "Ask me a yes or no question, or type quit to exit the program at any time."
+    puts "Welcome to the Scientifically Accurate 8 Ball. Absolutely No Magic Here!".colorize(:cyan)
+    puts "Ask me a yes or no question, or type quit to exit the program at any time.".colorize(:green)
     puts "---"
     ask_me
+end
+
+def random_pick
+    pick = @random_colors.sample
+    puts @answer_key.sample.colorize(pick)
 end
 
 def ask_me
@@ -24,7 +29,11 @@ def ask_me
         puts @answer_key
         ask_me
     else
-        puts @answer_key.sample(1)
+        puts "..."
+        sleep(1)
+        puts "..."
+        sleep(1)
+        random_pick
         ask_me
     end
 end
@@ -35,11 +44,11 @@ def new_answerkey
 end
 
 def add_answers
-    puts "Which answer would you like to add? (or type ask to ask a new question)"
+    puts "Which answer would you like to add? (or type ask to ask a new question)".colorize(:white)
     nuanswer = gets.strip
     case nuanswer
     when "Yes"; "No"; "Keep Dreaming Cueball"; "You Better Put Your Lucky Socks On"; "HAHA! NOOB! Definitely Not"; "Yeah, I could see that"; "Maybe in another Dimension"; "You Win, Everytime, With No Exeption"
-        puts "That answer already exist, try again"
+        puts "That answer already exist, try again".colorize(:red)
         puts "---"
         add_answers
     when "ask"
@@ -47,7 +56,7 @@ def add_answers
     when "quit"
         exit
     else
-        puts "Your answer has been added to the Scientifcally Accurate 8 Ball."
+        puts "Your answer has been added to the Scientifcally Accurate 8 Ball.".colorize(:green)
         nuanswer = @answer_key.push(nuanswer)
     ask_me
     end
@@ -61,5 +70,5 @@ end
 #puts "Yeah, I Could See That"
 #puts "Maybe In Another Dimension"
 #puts "You Win, Every Time, No Exceptions"
-
+#random_pick
 launch
